396924fc9cd345b614f19818c66a802f3b89dbbd
eltonjncorreia <eltonjncorreia@gmail.com>
2021-07-22 06:38:49 -0300

    remove action in file db

--- api/db.go
+++ api/db.go
@@ -9,7 +9,7 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func connection() (*sql.DB, error) {
+func Connection() (*sql.DB, error) {
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Println("Error loading .env file")
@@ -27,23 +27,4 @@ func connection() (*sql.DB, error) {
 	}
 
 	return db, nil
-}
-
-func CreateClient(c Client) (string, error) {
-	var lastID string
-	var stmt string
-
-	db, err := connection()
-	if err != nil {
-		return "", err
-	}
-	defer db.Close()
-
-	stmt = "insert into clients (nome, endereco, cadastrado_em, atualizado_em) values($1, $2, $3, $4) RETURNING uuid"
-
-	erro := db.QueryRow(stmt, c.Nome, c.Endereco, c.Cadastrado_em, c.Atualizado_em).Scan(&lastID)
-	if erro != nil {
-		return "", erro
-	}
-	return lastID, nil
 }
